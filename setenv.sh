9fcf3100294a1bf070111121a9acfa436bf2275d
Michael Südkamp <michael.suedkamp@quanos-solutions.com>
2021-10-23 19:16:36 +0200

    set environment (probably irrelevant for Eclipse setup)

---
+++ setenv.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# according to the tutorial README the following environment variables should be set. But I cannot imagine that they are relevant if you setup the tutorial with Eclipse.
+
+export JAVA_HOME="D:/java/OpenJDK11U-jdk_x64_windows_hotspot_11.0.8_10"
+export PATH="D:/java/OpenJDK11U-jdk_x64_windows_hotspot_11.0.8_10/bin"
